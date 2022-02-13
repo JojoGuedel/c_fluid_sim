099bfe58267c97a3a6fa8f2873f5c06664f39774
JojoGuedel <67550456+JojoGuedel@users.noreply.github.com>
2022-02-13 08:56:10 +0100

    main.c: implemented stable diffuser

--- src/main.c
+++ src/main.c
@@ -20,13 +20,13 @@ void fluid_add_source(FluidCell *fluid_target, int width, int x, int y, float so
 
 void fluid_set_boundaries(FluidCell *fluid_target, int width, int height)
 {
-    for(int x = 0; x < width; x++)
+    for (int x = 0; x < width; x++)
     {
         fluid_target[x + 0] = fluid_target[x + width];
         // TODO: isn't there a boundary missing?
     }
 
-    for(int y = 0; y < height; y++)
+    for (int y = 0; y < height; y++)
     {
         fluid_target[0         + y * width] = fluid_target[1         + y * width];
         fluid_target[width - 1 + y * width] = fluid_target[width - 2 + y * width];
@@ -37,8 +37,8 @@ void fluid_diffuse_bad(FluidCell *fluid_target, FluidCell *fluid_src, int width,
 {
     float diff_rate_dt = diff_rate * delta_time /** width * height*/;
 
-    for(int y = 1; y < height - 1; y++)
-        for(int x = 1; x < width - 1; x++)
+    for (int y = 1; y < height - 1; y++)
+        for (int x = 1; x < width - 1; x++)
         {
             fluid_target[x + y * width].density = fluid_src[(x    ) + (y    ) * width].density + diff_rate_dt * (
                                                   fluid_src[(x + 1) + (y    ) * width].density +
@@ -46,8 +46,25 @@ void fluid_diffuse_bad(FluidCell *fluid_target, FluidCell *fluid_src, int width,
                                                   fluid_src[(x    ) + (y + 1) * width].density +
                                                   fluid_src[(x    ) + (y - 1) * width].density - 4.0f * 
                                                   fluid_src[(x    ) + (y    ) * width].density);
-            // log_info("%f, %f", diff_rate_dt, fluid_target[x + y * width].density);
         }
+    
+    // fluid_set_boundaries(fluid_target, width, height);
+}
+
+void fluid_diffuse(FluidCell *fluid_target, FluidCell *fluid_src, int width, int height, float diff_rate, int percision)
+{
+    float diff_rate_dt = diff_rate * delta_time /** width * height*/;
+
+    for (int p = 0; p < percision; p++)
+        for (int y = 1; y < height - 1; y++)
+            for (int x = 1; x < width - 1; x++)
+            {
+                fluid_target[x + y * width].density = (  fluid_src[(x    ) + (y    ) * width].density + diff_rate_dt * (
+                                                      fluid_target[(x + 1) + (y    ) * width].density +
+                                                      fluid_target[(x - 1) + (y    ) * width].density +
+                                                      fluid_target[(x    ) + (y + 1) * width].density +
+                                                      fluid_target[(x    ) + (y - 1) * width].density)) / (1 + 4.0f * diff_rate_dt);
+            }
     
     // fluid_set_boundaries(fluid_target, width, height);
 }
@@ -63,26 +80,28 @@ FluidCell *back    = fluid_field_b2;
 
 void on_create()
 {
-    for(int i = 0; i < (WIDTH + 2) * (HEIGHT + 2); i++)
+    for (int i = 0; i < (WIDTH + 2) * (HEIGHT + 2); i++)
         current[i] = (FluidCell) {0, 0, 255};
 }
 
 void on_update()
 {
-    fluid_diffuse_bad(back, current, (WIDTH + 2), (HEIGHT + 2), 10.0f);
+    // fluid_diffuse_bad(back, current, (WIDTH + 2), (HEIGHT + 2), 10.0f);
+    fluid_diffuse    (back, current, (WIDTH + 2), (HEIGHT + 2), 10.0f, 40);
 
     // "swap the buffers"
     FluidCell *temp = current;
     current = back;
     back = temp;
 
-    for(int y = 0; y < HEIGHT; y++)
-        for(int x = 0; x < WIDTH; x++)
+    for (int y = 0; y < HEIGHT; y++)
+        for (int x = 0; x < WIDTH; x++)
         {
             color((Color){x, y, current[(x + 1) + (y + 1) * (WIDTH + 2)].density, 255});
             draw_pixel(x, y);
-            // log_info("%i, %i: %f", x, y, current[(x + 1) + (y + 1) * (WIDTH + 2)].density);
         }
+    
+    log_info("%f", 1.0f / delta_time);
 }
 
 int main()
